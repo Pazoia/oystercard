ded6288be94836faafd446a4fe496ca3dae202ae
Paulo Azoia <pazoia.gomes@gmail.com>
2020-01-31 16:01:24 +0000

    Step 14, Added entry_st and Exit_st instance vars to Journey class

--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -2,4 +2,13 @@ require 'journey'
 
 describe Journey do
 
+  journey = Journey.new("start_st", "end_st")
+
+  it 'saves entry_station name' do
+    expect(journey.entry_station).to eq("start_st")
+  end
+  
+  it 'saves exit_station name' do
+    expect(journey.exit_station).to eq("end_st")
+  end
 end
